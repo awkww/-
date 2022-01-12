b454a3ba986b9fbe1db524a18162ed4da1505b61
awkww <97575456+awkww@users.noreply.github.com>
2022-01-12 12:06:33 +0900

    Add files via upload

---
+++ dd.py
@@ -0,0 +1,61 @@
+#주석 - 메모
+
+# 더하기 빼기 곱하기 나누기 나머지 몫
+# + - * / % //
+
+5%3
+
+#출력함수 print()
+#print(2*5%3)
+#변수
+#변수의 이름 = 들어갈 값
+# a=2+3
+# b=7*8
+# c=a+b
+# a=b
+
+#
+
+#
+# a=7
+# b=13
+# c=a
+# a=b
+# b=c
+# print(a,b)
+
+# a=3
+# b=6
+# c=9
+# d=a
+# e=b
+# f=c
+# b=d
+# c=e
+# a=f
+# print(a,b,c)
+
+#자료형
+# a=41  #int - 정수형
+# b=3.1 #float - 실수형
+# c="abcd" #str - 문자열
+#
+# #type() - 들어간 값의 자료형을 알려줌
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+# a="abc"
+# b="가나다"
+# c=a+b+"!"
+# print(a+b)
+#
+# "1234" #문자
+
+#인덱싱, 슬라이싱
+a= "hello python!" #0부터 시작  #공백도 세기
+print(a[1])
+print(a[7:])   #이상
+print(a[:4])   #미만
+print(a[3:9])
